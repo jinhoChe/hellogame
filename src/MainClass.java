4176bbddc0037a9ef7591f65d3412d6991e524a7
cjh <rhtnxndbdb@gmail.com>
2022-06-28 12:05:08 +0900

    메소드 데이터 전달

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,11 +7,16 @@ public class MainClass {
         // 클래스를 불러서 사용
         NameSet ns = new NameSet();
         ns.setName(); // 클래스이름 . < 붙이기 (이름 설정 기능
+//        System.out.println(ns.n+"꺼져~");
+//        ns.n = "오늘?";
+//        System.out.println(ns.n+"이리와~");
+
         // 이름을 출력
-        ns.getName(); //(이름 출력 기능
+        String n = ns.getName(); //(이름 출력 기능 , 설정된 이름을 받을 수 있다.
         // 플레이~ 시작
         PlayClass pc = new PlayClass();
         //pc.play1(안에서는 쌍따옴표>>""만 치면 앞에있는n을 가져옴)
-        pc.play1("도라에몽");
+        // 투명한건 따라치는거 아님 ㅋ
+        pc.play1(n);
     }
 }
--- src/NameSet.java
+++ src/NameSet.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 /*
 * 이름을 설정하는 클래스
  */
 public class NameSet {
     // 메소드 = public void 메소드이름(){ }
+    private String n; //멋대로 사용방지 private
     public  void  setName(){
         System.out.println("이름 설정 기능");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("이름 설정:");
+        n = sc.next();//문자열 입력 받기 String n <<저장소 생성
     }// 이름을 갖고와서 출력하는 기능
-    public  void  getName(){
+    public String getName(){
         System.out.println("이름 출력 기능");
+        System.out.println(n+"아 안녕!!!!!!!!!!!");
+        return n;
     }
 }
